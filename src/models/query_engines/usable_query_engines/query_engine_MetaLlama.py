392b8ebe50c530dd0303b3cb3c68bf8f04b362fb
David <treadwell.d@northeastern.edu>
2025-04-09 13:33:31 -0700

    Add Meta-Llama query engine

---
+++ src/models/query_engines/usable_query_engines/query_engine_MetaLlama.py
@@ -0,0 +1,40 @@
+# David Treadwell
+# CS 7180 - Generative AI
+# treadwell.d@northeastern.edu
+# query_engine_MetaLlama.py - Child class to use meta-llama/llama-3.2-3b-instruct:free model as query engine
+
+from src.models.query_engines.query_engine_base_class.query_engine_base import QueryEngineBase
+from llama_index.llms.openrouter import OpenRouter
+import os
+
+
+class QueryEngineMetaLlama(QueryEngineBase):
+    """
+    Usable query engine with the meta-llama/llama-3.2-3b-instruct:free LLM model
+    """
+    def __init__(self, nodes=None, embed_model_name="sentence-transformers/all-MiniLM-L6-v2"):
+        # Load LLM and put in Settings
+        self._model_name = "meta-llama/llama-3.2-3b-instruct"
+        self._llm = self.load_llm()
+
+        # Instantiate super class
+        super().__init__(nodes=nodes, llm=self._llm, embed_model_name=embed_model_name)
+
+    def load_llm(self):
+        """
+        Loads an LLM
+        :return: The llm being used by the query engine after loading
+        """
+        api_key = os.getenv("OPENROUTER_API_KEY")
+        if not api_key:
+            raise ValueError("Missing OpenRouter API key. "
+                             "Set the 'OPENROUTER_API_KEY' environment variable in your \'.env\' file.")
+        llm = OpenRouter(api_key=api_key, model=self._model_name, max_tokens=512, context_window=4096)
+        return llm
+
+    def get_llm(self):
+        """
+        Returns the LLM being used by the Query Engine
+        :return: Current LLM model
+        """
+        return self._llm
--- tests/test_RAG_patient_data.py
+++ tests/test_RAG_patient_data.py
@@ -5,6 +5,7 @@ from dotenv import load_dotenv
 from src.service.node_manager import NodeManager
 from src.models.query_engines.usable_query_engines.query_engine_mistral import QueryEngineMistral
 from src.models.query_engines.usable_query_engines.query_engine_qwen import QueryEngineQwen
+from src.models.query_engines.usable_query_engines.query_engine_MetaLlama import QueryEngineMetaLlama
 
 
 def main(argv):
@@ -25,7 +26,7 @@ def main(argv):
     nodes = node_manager.get_nodes()
 
     # Run test query
-    query_engine = QueryEngineQwen(nodes)
+    query_engine = QueryEngineMetaLlama(nodes)
     query = "What is the patient's father medical history?"
     print(query_engine.generate_response(query))
 
